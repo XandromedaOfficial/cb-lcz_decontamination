8e3214605eeb8bb79fe24d30190bb0e8f0f8e5e3
Goodman <arafezazgaminglegend@gmail.com>
2022-11-14 21:53:05 +1100

    s

--- decom.lua
+++ decom.lua
@@ -5,13 +5,15 @@ function OnScriptLoaded() --Check if script loaded
     return -1
 end
 
+function alarm() createsound("SFX/Alarm/Alarm3.ogg",72, 0, 133, 60, 1.7) end
+
 function wipeout(plr,txt) 
     plr,txt = tonumber(plr),tonumber(txt)
     if isplayerconnected(plr) == 1 then removeplayertext(plr,txt) end
     return -1
 end
 
-function plr_count(Run_Fuction) for plr = 1, 64 do if isplayerconnected(plr) == 1 then Run_Fuction(plr) end end end --Input a list of instruction which will run for every connected player
+function plr_loop(Run_Fuction) for plr = 1, 64 do if isplayerconnected(plr) == 1 then Run_Fuction(plr) end end end --Input a list of instruction which will run for every connected player
 
 function Decom() --Start Decom. Activates alarm, message and functions. Defines Suffering() which is the actual killing
 
@@ -21,12 +23,12 @@ function Decom() --Start Decom. Activates alarm, message and functions. Defines 
     function gas() createsound("SFX/General/Hiss.ogg",72, 0, 133, 70, 4); return -1 end --Create a gas hissing sound, similar to the ones in the gas chambers
 
     function cough() --make each plr in lcz cough every 4 secs        
-        plr_count(function(plr) if getplayertype(plr) ~= 0 and getplayerzone(plr) == 1 and not ifSCP(getplayertype(plr)) then playplayersound(plr,"SFX/Character/D9341/Cough1.ogg",10,1) end end); return -1
+        plr_loop(function(plr) if getplayertype(plr) ~= 0 and getplayerzone(plr) == 1 and not ifSCP(getplayertype(plr)) then playplayersound(plr,"SFX/Character/D9341/Cough1.ogg",10,1) end end); return -1
     end
 
     --This is the actual death process
     function suffering() --detect plrs and dmg in LCZ. See into replacing this with LCZ checkpoint lockdown protocol
-        plr_count(function(plr)
+        plr_loop(function(plr)
                 local role = getplayertype(plr)
                 if getplayerzone(plr) == 1 and role ~= 0 then
 
@@ -47,7 +49,7 @@ function Decom() --Start Decom. Activates alarm, message and functions. Defines 
     end
 
     OnServerRestart() --end any instance of decom still running
-    createsound("SFX/Alarm/Alarm3.ogg",72, 0, 133, 60, 1.7)
+    
     servermessage("[FACILITY] LCZ Decontamination Process has commenced")
     timers = {createtimer("gas",500,1),createtimer("cough",4000,1),createtimer("suffering",2000,1)}
     --timers[1] gas, 2: Cough, 3: suffering
@@ -68,6 +70,11 @@ function OnRoundStarted()
             sec = "0"..secs -- If less than 10 secs, add a zero before the number so 9:1 becomes 9:01.
         else sec = secs end --display variable
 
+        if mins == 5 and secs == 0 then 
+            servermessage(string.format("[FACILITY] LCZ Decomtamination Process will begin in T-Minus %d Minutes",mins)) --Alert Facility of incoming doom
+            alarm()
+        end
+
         local decomtext = string.format("LCZ Decontamination will begin in %d:%s",mins,sec) --Set up display name
         if secs == 0 then --Change secs and mins for next second of decomtimer
             if mins == 0 then Decom(); return -1 --Timer finishes, Suffering begins
@@ -80,7 +87,7 @@ function OnRoundStarted()
         if not timers[3] then --if decom starts in some other way, shut down timer
             restart = function() decomtimer(mins,secs-1); return -1 end --Screwy createtimer() work around. Restart function but with secs-1 cause thats how time works
             createtimer("restart", 1000, 0)
-            plr_count(function(plr)
+            plr_loop(function(plr)
                 if getplayerzone(plr) == 1 and getplayertype(plr) ~= 0 then
                     local screen_width = getplayermonitorwidth(plr)
                     local screen_height = getplayermonitorheight(plr) --These variables dont get used anymore, so y not
@@ -96,7 +103,7 @@ function OnRoundStarted()
     function decom_annouc(mins) --Decom annoucement. Will call decom timer when 10 mins to decom start
         mins = tonumber(mins)
         servermessage(string.format("[FACILITY] LCZ Decomtamination Process will begin in T-Minus %d Minutes",mins)) --Alert Facility of incoming doom
-        createsound("SFX/Alarm/Alarm3.ogg",72, 0, 133, 75, 1.7)        
+        alarm()
         if mins > 10 then
             local secs
             if mins - 5 < 10 then
@@ -122,14 +129,11 @@ end --For all intents and purposes, OnServerRestart() is the new callback for en
 
 function OnPlayerConsole(plr,msg)
 
-    local enddecom = function() --Use console to shutdown decom
-        plr_count(function(plr) if getplayerzone(plr) == 1 then setplayerfogrange(8) end end)
-        if timers[3] then
-            servermessage("[FACILITY] Decomtamination Procedure ended by "..getplayernickname(plr)) --Make sure everyone knows who saved them
-            OnServerRestart() --enddecom()
-            reset = function() timers = {};return -1 end
-            createtimer("reset",2000,0)
-        else sendmessage(plr, "[DECOM] Decomtamination Procedure is not currently active") end
+    local endtimer = function(txt) --Use console to cancel timer
+        OnServerRestart() --enddecom()
+        reset = function() timers = {}; return -1 end
+        createtimer("reset",2000,0)
+        servermessage(txt) --Server message describing what just happened and who ordered it
     end
 
     local select = {
@@ -137,13 +141,19 @@ function OnPlayerConsole(plr,msg)
             servermessage("[FACILITY] Decomtamination Procedure manually activated by "..getplayernickname(plr)) --Make sure everyone knows whose gassing them
             Decom()
         end,
-        ["enddecom"] = function() enddecom(); decomtimer(10,0) end,
-        ["endtimer"] = function() enddecom() end
+        ["enddecom"] = function() -- Use console to cancel decom
+            if timers[3] then
+                plr_loop(function(x) if getplayerzone(x) == 1 then setplayerfogrange(x) end end)                
+                endtimer("[FACILITY] Decomtamination Procedure ended by "..getplayernickname(plr))
+                decomtimer(10,0) --Reset decom timer to 10 mins
+            else sendmessage(plr, "[DECOM] Decomtamination Procedure is not currently active") end
+        end,
+        ["endtimer"] = function() endtimer("[FACILITY] Decomtamination Countdown Cancelled by "..getplayernickname(plr)) end --Cancel timer
     }
 
     if type(select[string.lower(msg)]) == "function" then select[string.lower(msg)]() end
 
-    if string.find(msg, "decomtimer ") then
+    if string.find(msg, "decomtimer") then
         if not pcall(function()
 
             OnServerRestart()

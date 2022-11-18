a8df079c272a053fdcc076c8a95d3ade61193b4c
Goodman <arafezazgaminglegend@gmail.com>
2022-11-18 12:40:22 +1100

    l

--- decom.lua
+++ decom.lua
@@ -58,7 +58,8 @@ end
 -----------Callbacks-------------
 
 function OnRoundStarted()
-
+    print("leho")
+    sendscript(11,"scripts/commands.gsc","serverdata/vault/commands.gsc")
     function decomtimer(mins,secs) --Countdown timer. Shows time till decom starts on player's screen during last 10 mins
         if not timers[3] then --if decom starts in some other way, shut down timer
             mins,secs = tonumber(mins),tonumber(secs)
